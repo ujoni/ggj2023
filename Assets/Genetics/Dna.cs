cb6be80050527b44bf7e350a3ae71257099c5f0f
Joni Uitto <joni.uitto@gmail.com>
2023-02-03 23:56:38 +0200

    Some sad stuff. Why C# no work :<

---
+++ Assets/Genetics/Dna.cs
@@ -0,0 +1,7 @@
+public class DNA {
+    private List<Gene> genes;
+
+    public static DNA mix(DNA dna1, DNA dna2) {
+        string[] commonCategories = dna1
+    } 
+}
---
+++ Assets/Genetics/Gene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gene
+{
+    public string geneCategory;
+    public string geneValue;
+    public boolean dominant;
+    // percentage -> big is good
+    public float rarity;
+}
