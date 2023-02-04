6923534fd00c68febea45288b67ae4cd7a5db6b1
Joni Uitto <joni.uitto@gmail.com>
2023-02-05 01:19:35 +0200

    Some initial propositions work before sleeep

---
+++ Assets/Propositons/Propositions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propositions : MonoBehaviour
+{
+    public UnityEngine.Object PersonFab;
+    public GameObject root;
+    // Start is called before the first frame update
+
+    public void GenerateNewPropositions()
+    {
+
+    }
+}
