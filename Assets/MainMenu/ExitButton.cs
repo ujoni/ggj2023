b4ef5c29eefc2ff0a7eae1e271ef4bcab7187276
Joni Uitto <joni.uitto@gmail.com>
2023-02-04 23:18:19 +0200

    Main menu scene

---
+++ Assets/MainMenu/ExitButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
