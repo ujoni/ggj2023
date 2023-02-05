46d760fc9ff138770e77c2c030bb4cc58bf4fab3
Joni Uitto <joni.uitto@gmail.com>
2023-02-05 13:02:03 +0200

    Add simple trait for each phenotype
    Only select goal traits from those with two or more reqs, no overlaps

--- Assets/Genetics/Traits.cs
+++ Assets/Genetics/Traits.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using Mono.Cecil;
+using TMPro.SpriteAssetUtilities;
+using Unity.Burst;
 using UnityEngine;
 using UnityEngine.Assertions.Comparers;
+using UnityEngine.UIElements;
 using static GeneData;
 public class TraitRequirement
 {
@@ -52,40 +55,54 @@ public class Trait
             .Select(req => 1f - ProbabilityToInherit[req.category])
             .Aggregate((total, cur) => total + cur));
     }
+
+    public bool SharesGeneCategory(Trait withAnother)
+    {
+        var otherCategories = withAnother.requirements.Select(req => req.category).ToList();
+        return requirements.Select(req => otherCategories.Contains(req.category)).Any(val => val);
+    }
 }
 
 public static class Traits
 {
-    private static bool sorted = false;
     public static readonly List<Trait> traits = new(){
         // if you want to add only one requirement, the type must be specified.
         // with multiple reqs, the type inference works just fine
 
-        // singular requirement traits
+        // singular requirement traits (tied to dominant alleles only)
         // LOGIC
         new("Slow-witted", new TraitRequirement(GeneCategory.R_Logic_5, "A")),
         new("Smart", new TraitRequirement(GeneCategory.R_Logic_5, "b")),
+        new("Quick-witted", new TraitRequirement(GeneCategory.R_Logic_5, "bc")),
+        new("Logical", new TraitRequirement(GeneCategory.R_Logic_5, "cb")),
+        new("Irrational", new TraitRequirement(GeneCategory.R_Logic_5, "c")),
         // EMPATHY
         new("Caring", new TraitRequirement(GeneCategory.R_Empathy_2, "D")),
         new("Indifferent", new TraitRequirement(GeneCategory.R_Empathy_2, "e")),
         // PHYSIQUE
         new("Weak", new TraitRequirement(GeneCategory.R_Physique_5, "F")),
         new("Strong", new TraitRequirement(GeneCategory.R_Physique_5, "FH")),
-        new("Muscular", new TraitRequirement(GeneCategory.R_Physique_5, "g")),
-        // CREATIVITY
+        new("Agile", new TraitRequirement(GeneCategory.R_Physique_5, "g")),
+        new("Fast", new TraitRequirement(GeneCategory.R_Physique_5, "H")),
+        new("Slow", new TraitRequirement(GeneCategory.R_Physique_5, "HF")),
+        // CREATIVITY + x
         new("Creative", new TraitRequirement(GeneCategory.Creativity_4, "I")),
         new("Absent-minded", new TraitRequirement(GeneCategory.Creativity_4, "IJ")),
+        new("Industrious", new TraitRequirement(GeneCategory.Creativity_4, "JI")),
+        new("Lazy", new TraitRequirement(GeneCategory.Creativity_4, "J")),
         new("Sociopath", new TraitRequirement(GeneCategory.Creativity_4, "x")),
-        // CHARISMA
+        // CHARISMA + x
         new("Extrovert", new TraitRequirement(GeneCategory.Charisma_2, "K")),
         new("Introvert", new TraitRequirement(GeneCategory.Charisma_2, "l")),
         new("Hideous", new TraitRequirement(GeneCategory.Charisma_2, "x")),
-        // PIETY
+        // PIETY + x
         new("Single-minded", new TraitRequirement(GeneCategory.Piety_5, "M")),
         new("Fathful", new TraitRequirement(GeneCategory.Piety_5, "n")),
-        new("Fervent", new TraitRequirement(GeneCategory.Piety_5, "p")),
+        new("Fervent", new TraitRequirement(GeneCategory.Piety_5, "np")),
+        new("Untrusting", new TraitRequirement(GeneCategory.Piety_5, "pn")),
+        new("Loyal", new TraitRequirement(GeneCategory.Piety_5, "p")),
         new("Delusional", new TraitRequirement(GeneCategory.Piety_5, "x")),
-        // QUIRKS
+        // QUIRKS + x 
         new("Fairly average", new TraitRequirement(GeneCategory.Quirks_10, "Q")),
         new("Large-nosed", new TraitRequirement(GeneCategory.Quirks_10, "r")),
         new("Fidgety", new TraitRequirement(GeneCategory.Quirks_10, "rs")),
@@ -97,8 +114,18 @@ public static class Traits
         new("Long-armed", new TraitRequirement(GeneCategory.Quirks_10, "ts")),
         new("Lost", new TraitRequirement(GeneCategory.Quirks_10, "t")),
         new("Two-headed", new TraitRequirement(GeneCategory.Quirks_10, "x")),
+        // Singularity + x
+        new("Unique", new TraitRequirement(GeneCategory.Singularity_1, "u")),
+        new("Outlandish", new TraitRequirement(GeneCategory.Singularity_1, "x")),
+
         
-        // complex traits
+        // complex traits between vital traits (logic, empathy, physique)
+        new("Manipulative", new(GeneCategory.R_Logic_5, "b"), new(GeneCategory.R_Empathy_2, "D")),
+        new("People-person", new(GeneCategory.R_Logic_5, "bc"), new(GeneCategory.R_Empathy_2, "e")),
+        new("Competitive", new(GeneCategory.R_Logic_5, "cb"), new(GeneCategory.R_Physique_5, "FH")),
+        new("Ferocious", new(GeneCategory.R_Logic_5, "c"), new(GeneCategory.R_Physique_5, "H")),
+        new("Approachable", new(GeneCategory.R_Empathy_2, ""), new(GeneCategory.R_Physique_5, "")),
+        new("Forgettable", new(GeneCategory.R_Empathy_2, ""), new(GeneCategory.R_Physique_5, "")),
 
     };
 
@@ -107,20 +134,20 @@ public static class Traits
         return traits.Where(trait => trait.PresentIn(dna)).ToList();
     }
 
-    public static List<Trait> GetRandomGoalTraits(int count = 3)
+    public static List<Trait> GetRandomGoalTraits(int count = 2)
     {
-        var realCount = count < 1 ? 3 : count;
-        if (!sorted)
-        {
-            sorted = true;
-            traits.Sort((trait1, trait2) => trait2.GetValue() - trait1.GetValue());
-        }
+        var realCount = count < 1 ? 2 : count;
+        var traitsWithTwoOrMoreReqs = traits.Where(trait => trait.requirements.Count > 1).ToList();
+        traitsWithTwoOrMoreReqs.Sort((trait1, trait2) => trait2.GetValue() - trait1.GetValue());
+
         List<Trait> ret = new();
-        for (int i = 0; i < realCount; i++)
+        while (ret.Count < realCount)
         {
             // favors traits closer to the beginning
-            var index = (int)(Mathf.Pow(UnityEngine.Random.Range(0f, 1f), 2) * traits.Count);
-            ret.Add(traits[index]);
+            var index = (int)(Mathf.Pow(UnityEngine.Random.Range(0f, 1f), 2) * traitsWithTwoOrMoreReqs.Count);
+            var selected = traitsWithTwoOrMoreReqs[index];
+            if (ret.Any(trait => trait.SharesGeneCategory(selected))) continue;
+            ret.Add(selected);
         }
         return ret;
     }
